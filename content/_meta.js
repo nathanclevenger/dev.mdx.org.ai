1e49b5e9dea62340bd694ac7aa4b4cd8ddea7f04
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2024-12-13 08:24:43 -0600

    Not sure the build issue

--- content/_meta.js
+++ content/_meta.js
@@ -1,10 +1,10 @@
 export default {
   index: 'MDX',
-  AI: 'Generative AI',
-  Data: 'Structured Data',
-  Content: 'Unstructured Content',
-  Code: 'Executable Code',
-  UI: 'UI Components',
+  // AI: 'Generative AI',
+  // Data: 'Structured Data',
+  // Content: 'Unstructured Content',
+  // Code: 'Executable Code',
+  // UI: 'UI Components',
   '---': {
     type: 'separator'
   },
